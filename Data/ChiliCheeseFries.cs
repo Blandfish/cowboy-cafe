44042c31c9334ee1142bf031c011ee711fa2b8ec
Ankruse10 <Ankruse10@F1092-04-2092>
2020-02-03 15:14:57 -0600

    started on ChiliCheeseFries.cs

--- Data/ChiliCheeseFries.cs
+++ Data/ChiliCheeseFries.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace CowboyCafe.Data
 {
-    class ChiliCheeseFries
+    class ChiliCheeseFries : side
     {
+        public override uint Calories
+        {
+            get
+            {
+                switch(Size)
+                {
+                    case Size.Small:
+                        return 433;
+                        break;
+                    case Size.Medium:
+                        return 524;
+                        break;
+                    case Size.Large:
+                        return 610;
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+        }
     }
 }
