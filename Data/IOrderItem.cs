5da4263e8a7ccb8467fc3de5f1d9702675d3461f
Ankruse10 <Ankruse10@F1092-04-2092>
2020-02-26 14:50:14 -0600

    2/26

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// an interface representing a singel item in an order
+    /// </summary>
+    public interface IOrderItem
+    {
+        double Price { get; }
+
+        List<string> SpecialInstructions { get; }
+    }
+}
