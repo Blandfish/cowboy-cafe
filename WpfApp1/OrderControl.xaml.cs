acaa535cf1704d993483e9edeffaad5ac74b1e64
Ankruse10 <Ankruse10@E1119W03>
2020-03-04 12:08:06 -0600

    ordersummary control wrorl

--- WpfApp1/OrderControl.xaml.cs
+++ WpfApp1/OrderControl.xaml.cs
@@ -30,104 +30,10 @@ namespace PointOfSale
             Order CurrentOrder = new Order();
             DataContext = CurrentOrder;
 
-            
-            AddCowpokeChiliButton.Click += OnAddCowpokeChiliButtonClicked;
-           /* AddAngryChickenButton.Click += OnAddAngryChickenButtonClicked;
-            AddBakedBeansButton.Click += OnAddBakedBeansButtonClicked;
-            AddCornDodgersButton.Click += OnAddCornDodgersButtonClicked;
-            AddDakotaDoubleBurgerButton.Click += OnAddDakotaDoubleBurgerButtonClicked;
-            AddPanDeCampoButton.Click += OnAddPanDeCampoButtonClicked;
-            AddPecosPulledPorkButton.Click += OnAddPecosPulledPorkButtonClicked;
-            AddRustlersRibsButton.Click += OnAddRustlersRibsButtonClicked;
-            AddTexasTripleBurgerButton.Click += OnAddTexasTripleBurgerButtonClicked;
-            AddTrailBurgerButton.Click += OnAddTrailBurgerButtonClicked;
-            AddChiliCheeseFriesButton.Click += OnAddChiliCheeseFriesButtonClicked;
-
-            AddTexasTeaButton.Click += OnAddTexasTeaButtonClicked;
-            AddWaterButton.Click += OnAddWaterButtonClicked;
-            AddJerkedSodaButton.Click += OnAddJerkedSodaButtonClicked;
-            AddCowboyCoffeeButton.Click += OnAddCowboyCoffeeButtonClicked;
-             */
+           
         }
 
 
-        void OnAddCowpokeChiliButtonClicked(Object sender, RoutedEventArgs e)
-        {
-
-            ((Order)DataContext).Add(new CowpokeChili());
-                
-        }        /*
-        void OnAddAngryChickenButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.Items.Add(new AngryChicken());
-        }
-
-        void OnAddBakedBeansButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.Items.Add(new BakedBeans());
-        }
-
-        void OnAddChiliCheeseFriesButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.Items.Add(new ChiliCheeseFries());
-        }
-
-        void OnAddCornDodgersButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.Items.Add(new CornDodgers());
-        }
-
-        void OnAddDakotaDoubleBurgerButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.Items.Add(new DakotaDoubleBurger());
-        }
-
-        void OnAddJerkedSodaButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.Items.Add(new JerkedSoda());
-        }
-
-        void OnAddPanDeCampoButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.Items.Add(new PanDeCampo());
-        }
-
-        void OnAddPecosPulledPorkButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.Items.Add(new PecosPulledPork());
-        }
-
-        void OnAddRustlersRibsButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.Items.Add(new RustlersRibs());
-        }
-
-        void OnAddTexasTeaButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.Items.Add(new TexasTea());
-        }
-
-        void OnAddTexasTripleBurgerButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.Items.Add(new TexasTripleBurger());
-        }
-
-        void OnAddTrailBurgerButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.Items.Add(new TrailBurger());
-        }
-
-        void OnAddWaterButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.Items.Add(new Water());
-        }
-
-        void OnAddCowboyCoffeeButtonClicked(Object sender, RoutedEventArgs e)
-        {
-            OrderListView.DataContext.(new CowboyCoffee());
-        }
-
-        }
-        */
+        
     }
 }
